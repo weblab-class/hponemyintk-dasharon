3bc58d1d9214eae87eb563802acd5cd141cf1a83
dasharon <dsharon36@gmail.com>
2021-01-10 21:12:29 -0500

    Added catbook nav bar code

---
+++ client/src/components/modules/NavBar.js
@@ -0,0 +1,35 @@
+/*
+* From catbook-react code, many thanks to Web Lab team*
+*/
+
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+import "./NavBar.css";
+
+/**
+ * The navigation bar at the top of all pages. Takes no props.
+ */
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <nav className="NavBar-container">
+        <div className="NavBar-title u-inlineBlock">Catbook</div>
+        <div className="NavBar-linkContainer u-inlineBlock">
+          <Link to="/" className="NavBar-link">
+            Home
+          </Link>
+          <Link to="/profile/" className="NavBar-link">
+            Profile
+          </Link>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
