4e040f1ec79f06fc2b6aed8dbc5335643ebdc1a6
Kyaw <j.innocentboy@gmail.com>
2021-01-11 19:03:43 -0800

    Fun with Imaging Tagging

---
+++ client/src/components/modules/ReactAnnotate.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import Annotation from 'react-image-annotation'
+
+export default class Simple extends Component {
+  state = {
+    annotations: [],
+    annotation: {}
+  }
+
+  onChange = (annotation) => {
+    this.setState({ annotation })
+  }
+
+  onSubmit = (annotation) => {
+    const { geometry, data } = annotation
+
+    this.setState({
+      annotation: {},
+      annotations: this.state.annotations.concat({
+        geometry,
+        data: {
+          ...data,
+          id: Math.random()
+        }
+      })
+    })
+  }
+
+  render () {
+    return (
+      <Annotation
+        src={"https://upload.wikimedia.org/wikipedia/commons/6/61/F-35A_flight_%28cropped%29.jpg?download"}
+        alt='Two pebbles anthropomorphized holding hands'
+
+        annotations={this.state.annotations}
+
+        type={this.state.type}
+        value={this.state.annotation}
+        onChange={this.onChange}
+        onSubmit={this.onSubmit}
+        allowTouch
+      />
+    )
+  }
+}
