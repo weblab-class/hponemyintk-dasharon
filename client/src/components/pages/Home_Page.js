ccc4f864ed99b4837a2627d96310546e110cfde6
Kyaw <hponemyintk@gmail.com>
2021-01-19 13:09:01 -0500

    Added CSS styling to Home_Page

--- client/src/components/pages/Home_Page.js
+++ client/src/components/pages/Home_Page.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import "../../utilities.css";
 import "./Skeleton.css";
 
-
 class Home_Page extends Component {
   constructor(props) {
     super(props);
@@ -18,11 +17,14 @@ class Home_Page extends Component {
   render() {
     if (!this.props.userId) return <div>Goodbye! Thank you so much for using Weworld.</div>; //login protect
     return (
-      <>
-        {/* Use username prop */}
-        <p>Welcome {this.props.username}!</p>
-        <p>You have logged in! Hooray! Welcome!</p>
-      </>
+      <div className="u-flex u-flex-justifyCenter">
+        <div className="postColumn">
+          {/* Use username prop */}
+          <p>Welcome {this.props.username}!</p>
+          <p>You have logged in! Hooray! Welcome!</p>
+          <img src="https://powerlanguage.net/wp-content/uploads/2019/09/welcome.jpg" />
+        </div>
+      </div>
     );
   }
 }
