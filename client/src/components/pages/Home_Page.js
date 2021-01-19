cba39b5af10831bf04c4a033585983fce9f03baa
Kyaw <hponemyintk@gmail.com>
2021-01-19 17:48:06 -0500

    Added CSS styling to home

--- client/src/components/pages/Home_Page.js
+++ client/src/components/pages/Home_Page.js
@@ -10,9 +10,10 @@ class Home_Page extends Component {
   constructor(props) {
     super(props);
     // Initialize Default State
-    this.state = {photo_info_array: [], //this is a photo info array};
+    this.state = {
+      photo_info_array: [], //this is a photo info array};
+    };
   }
-}
 
   componentDidMount() {
     // remember -- api calls go here!, get call adapted from catbook
@@ -49,22 +50,24 @@ class Home_Page extends Component {
     // });
     //Find user whose photos we are seeing
 
-      get("/api/photosimpletestOnebyid", { photoId: "60075dbc90f80b3495af511d" }).then((ImageInfo_one) => {
+    get("/api/photosimpletestOnebyid", { photoId: "60075dbc90f80b3495af511d" }).then(
+      (ImageInfo_one) => {
         console.log(ImageInfo_one);
         this.setState({
           photo_info_array: [ImageInfo_one],
         });
-      });
-    }
+      }
+    );
+  };
 
-    //cleans up annotations
-    cleanAnnotInput = (initAnnotInput) => {
-      initAnnotInput.map((obj) => {
-        obj.geometry.type = obj.geometry.shape_kind; //[ref: renaming https://stackoverflow.com/questions/4647817/javascript-object-rename-key]
-        delete obj.geometry.shape_kind;
-      });
-      return initAnnotInput;
-    };
+  //cleans up annotations
+  cleanAnnotInput = (initAnnotInput) => {
+    initAnnotInput.map((obj) => {
+      obj.geometry.type = obj.geometry.shape_kind; //[ref: renaming https://stackoverflow.com/questions/4647817/javascript-object-rename-key]
+      delete obj.geometry.shape_kind;
+    });
+    return initAnnotInput;
+  };
 
   //give info on a first photo, now as text, would want to translate to picture/rating/annotation/etc.
   GetPhotoInfo(PhotoInfo) {
@@ -80,48 +83,112 @@ class Home_Page extends Component {
     // console.log(annotPhotoInfo);
 
     //multiple classes https://stackoverflow.com/questions/11918491/using-two-css-classes-on-one-element https://dev.to/drews256/ridiculously-easy-row-and-column-layouts-with-flexbox-1k01 helped with row and column, other refs in css file
-    return (            <ReactAnnotate
-      allowEdits={false}
-      border-radius="10%"
-      img_using={PhotoInfo.photo_placeholder}
-      annotationslst={annotPhotoInfo}
-      height="100"
-      width="100"
-    />)}
+    return (
+      <ReactAnnotate
+        allowEdits={false}
+        border-radius="10%"
+        img_using={PhotoInfo.photo_placeholder}
+        annotationslst={annotPhotoInfo}
+        height="100"
+        width="100"
+      />
+    );
+  }
 
   render() {
     if (!this.props.userId) return <div>Goodbye! Thank you so much for using Weworld.</div>; //login protect
     //tried https://www.w3schools.com/html/html_lists.asp for list but then decided not
     return (
       <div className="u-flex u-flex-justifyCenter">
-
         <div className="postColumn paddedText">
-        {this.state.photo_info_array ? (
-              <div>{this.state.photo_info_array.map((p) => this.GetPhotoInfo(p))}</div>) : (<p></p>)}
+          {this.state.photo_info_array ? (
+            <div className="u-flex u-flex-justifyCenter">
+              <div className="homeImg">
+                {this.state.photo_info_array.map((p) => this.GetPhotoInfo(p))}
+              </div>
+            </div>
+          ) : (
+            <p></p>
+          )}
           {/* Use username prop */}
           <p>Welcome {this.props.username}!</p>
           <p className="questiontext">What is WeWorld?</p>
-          <p className="answertext">WeWorld enables you to learn a language through your and others' photos. As you relate the language to your life through photo tags, you will learn and have fun!</p>
-          <p className="questiontext">Nice! I am excited to put my selfies to educational use. How can I get started learning from my photos?</p>
-          <p className="answertext">On the <Link className = "linktext" to="/Upload">Upload page</Link>, you upload your photos and add tags (currently you have to supply the translation, and we are hoping to implement translation!).
+          <p className="answertext">
+            WeWorld enables you to learn a language through your and others' photos. As you relate
+            the language to your life through photo tags, you will learn and have fun!
+          </p>
+          <p className="questiontext">
+            Nice! I am excited to put my selfies to educational use. How can I get started learning
+            from my photos?
+          </p>
+          <p className="answertext">
+            On the{" "}
+            <Link className="linktext" to="/Upload">
+              Upload page
+            </Link>
+            , you upload your photos and add tags (currently you have to supply the translation, and
+            we are hoping to implement translation!).
           </p>
           <p className="questiontext">Are my photos private?</p>
-          <p className="answertext">Please note currently all users can see everyone's content given this is an early testing version of the website. So please do not share any image or text you do not want shared publicly. Also your timestamp of use and name are recorded and associated with your image.</p>
-          <p className="questiontext">So once my photos are uploaded, how can I review them?</p>
-          <p className="answertext"> On the {this.props.userId && (
-                <Link to={`/Flashcards/${this.props.userId}`} className = "linktext">
-                  Review page
-                </Link>
-              )} you can scroll through all of your photos and review words- as well as your memories.
+          <p className="answertext">
+            Please note currently all users can see everyone's content given this is an early
+            testing version of the website. So please do not share any image or text you do not want
+            shared publicly. Also your timestamp of use and name are recorded and associated with
+            your image.
           </p>
-          <p className="questiontext">I'm excited to review, but I want a challenge and to really learn. Can I test myself?</p>
-          <p className="answertext">You will soon be able to! Our <Link to="/Quiz" className = "linktext">Quizzes page </Link>shows the quiz module (right now with a sample history lesson) which we hope to implement, where you will have to pick the word corresponding to a tag in a photo.</p>
-          <p className="questiontext">Now, you also said this is social? Can I see my friends' adorable pet* pictures and learn from them?</p>
-          <p className="answertext">Yup! The <Link to="/Friends" className = "linktext"> Social page </Link> shows a photo for each WeWorld user and has a link for you to see all of their photos.</p>
-          <p className="questiontext">What if I see troubling or inappropriate content uploaded by another user?</p>
-          <p className="answertext">WeWorld wants to ensure it is a welcoming and inclusive website. Please email us at weblab2021@gmail.com, so we can look into and address your concern.</p>
-          <p className="questiontext">I want inspiration, though! The idea of using my photos as a tool to help teach me a language is really new to me. How do I know which kinds of photos to upload?</p>
-          <p className="answertext">We are thinking of adding a <Link to="/Scavenger_Hunts" className = "linktext"> Scavenger Hunts page</Link>, which would provide photo prompts to get the learning and fun started!</p>
+          <p className="questiontext">So once my photos are uploaded, how can I review them?</p>
+          <p className="answertext">
+            {" "}
+            On the{" "}
+            {this.props.userId && (
+              <Link to={`/Flashcards/${this.props.userId}`} className="linktext">
+                Review page
+              </Link>
+            )}{" "}
+            you can scroll through all of your photos and review words- as well as your memories.
+          </p>
+          <p className="questiontext">
+            I'm excited to review, but I want a challenge and to really learn. Can I test myself?
+          </p>
+          <p className="answertext">
+            You will soon be able to! Our{" "}
+            <Link to="/Quiz" className="linktext">
+              Quizzes page{" "}
+            </Link>
+            shows the quiz module (right now with a sample history lesson) which we hope to
+            implement, where you will have to pick the word corresponding to a tag in a photo.
+          </p>
+          <p className="questiontext">
+            Now, you also said this is social? Can I see my friends' adorable pet* pictures and
+            learn from them?
+          </p>
+          <p className="answertext">
+            Yup! The{" "}
+            <Link to="/Friends" className="linktext">
+              {" "}
+              Social page{" "}
+            </Link>{" "}
+            shows a photo for each WeWorld user and has a link for you to see all of their photos.
+          </p>
+          <p className="questiontext">
+            What if I see troubling or inappropriate content uploaded by another user?
+          </p>
+          <p className="answertext">
+            WeWorld wants to ensure it is a welcoming and inclusive website. Please email us at
+            weblab2021@gmail.com, so we can look into and address your concern.
+          </p>
+          <p className="questiontext">
+            I want inspiration, though! The idea of using my photos as a tool to help teach me a
+            language is really new to me. How do I know which kinds of photos to upload?
+          </p>
+          <p className="answertext">
+            We are thinking of adding a{" "}
+            <Link to="/Scavenger_Hunts" className="linktext">
+              {" "}
+              Scavenger Hunts page
+            </Link>
+            , which would provide photo prompts to get the learning and fun started!
+          </p>
           <p>*WeWorld likes animals of all kinds, including both cats and dogs!</p>
           <p></p>
         </div>
