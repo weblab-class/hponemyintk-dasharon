d6c7aedf77c32be32d0e015293a985f244a92f62
dasharon <dsharon36@gmail.com>
2021-01-24 15:47:36 -0500

    Revised friends to only show one translate button. Now friends calls individual flashcard directly

--- client/src/components/pages/Friends_1251.js
+++ client/src/components/pages/Friends_1251.js
@@ -6,6 +6,7 @@ import { get } from "../../utilities";
 // import UserInfo from "../modules/UserInfo.js";
 import { Link } from "@reach/router";
 import View_Flashcards from "../pages/View_Flashcards.js";
+import IndividualFlashcard from "../modules/IndividualFlashcard.js";
 
 //This is an initial attempt at the friend page, to print users, hopefully leading to
 //Being able to add friends and see profiles
@@ -16,6 +17,8 @@ class Friends_1251 extends Component {
     // Initialize Default State
     this.state = {
       allUserList: [], //set initial user list to be empty
+      allPhotos: [],
+      showInNativeLanguage: false
     };
   }
 
@@ -36,22 +39,61 @@ class Friends_1251 extends Component {
     }
   }
 
-  getUsers = () => {
-    get("/api/all_user_find").then((allUserInfo) => {
-      this.setState({
-        allUserList: allUserInfo,
+  //async/await ref https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  getUsers = async() => {
+    try {
+    const allUsers = await get("/api/all_user_find");
+    this.setState({
+        allUserList: allUsers.filter(userCheck => userCheck.photoCount > 0)
       });
-    });
+    let allPhotoList = [];
+    for (let uu = 0; uu < this.state.allUserList.length; uu++)
+    {
+      console.log(uu, "USER INFO", this.state.allUserList[uu]);
+      const newPhoto = await get("/api/photosimpletestOne", { userId: this.state.allUserList[uu]._id });
+      console.log("PHOTO", newPhoto);
+      allPhotoList = allPhotoList.concat(newPhoto);
+    };
+    
+    this.setState({allPhotos : allPhotoList});
+    this.runFlip();
+    console.log("ALL PHOTOS", this.state.allPhotos)
+    }
+    catch (e) {console.log(e)}
   };
+  //     .then((allUsers) => get()).then(allPhotos => {this.setState({})})
+  //   });
+  // };
 
   //if requesting user's cards are being visualized, return my, otherwise return possessive of name
-  getPossessive = (reqUser, visUser, visUserName) => {
-    if (reqUser === visUser) {
-      return "my";
-    } else {
-      return visUserName + "'s";
-    }
-  };
+  // getPossessive = (reqUser, visUser, visUserName) => {
+  //   if (reqUser === visUser) {
+  //     return "my";
+  //   } else {
+  //     return visUserName + "'s";
+  //   }
+  // };
+
+    //on click flip to show in either native language or language learning ref https://stackoverflow.com/questions/12772494/how-to-get-opposite-boolean-value-of-variable-in-javascript/12772502
+    switchLanguage = (event) => {
+      this.setState({showInNativeLanguage : !this.state.showInNativeLanguage});
+      this.runFlip();
+    };
+
+    runFlip = () => {
+      //Flip the tags and printout languages in each annotation for each photo
+      for (let pp = 0; pp < this.state.allPhotos.length; pp++) 
+      {
+        for (let aa = 0; aa < this.state.allPhotos[pp].annotation_info_array.length; aa++)
+        {
+          const initialTag = this.state.allPhotos[pp].annotation_info_array[aa].data.text;
+          const initialText = this.state.allPhotos[pp].annotation_info_array[aa].data.textforBox;
+          this.state.allPhotos[pp].annotation_info_array[aa].data.text = initialText;
+          this.state.allPhotos[pp].annotation_info_array[aa].data.textforBox = initialTag;
+          console.log("in inner loop");
+        }
+      }
+};
 
   render() {
     //Chatbook login protection
@@ -59,7 +101,13 @@ class Friends_1251 extends Component {
     console.log(this.state.allUserList);
 
     //get all users who uploaded, JavaScript lecture slide 32, revised to check photoCount
-    let allUploadedUserList = this.state.allUserList.filter(userCheck => userCheck.photoCount > 0);
+    // let allUploadedUserList = this.state.allUserList.filter(userCheck => userCheck.photoCount > 0);
+
+    //If you are the requesting user, show "Me" instead of your name
+    //if (this.props.userId === this.state.requestingUserId) {this.setState({ nameForPrint :"Me"} )}else {this.setState({ nameForPrint : this.state.userName} )};
+    let langSwitchText = "Show comments and captions in language learning!";
+    if (this.state.showInNativeLanguage === false) {langSwitchText = "Show comments and captions in English!"}
+    
     return (
       <div className="u-flexColumn u-flex-alignCenter" style={{ width: "100%" }}>
         {/*Many thanks to Justin for Piazza link advice*/}
@@ -69,19 +117,33 @@ class Friends_1251 extends Component {
           Showing one photo from each user in the database! <br />
           (There are also links for showing all the photo from the users.)
         </h1>
-        {allUploadedUserList.map((u, i) => (
+        <button
+                type="button"
+                onClick={this.switchLanguage}
+                // style={{ border: "none", backgroundColor: "transparent" }}     //no longer need this as now styling with Image_aesthetics.css
+              > {langSwitchText}
+                {/* <FontAwesomeIcon icon={faTrashAlt} style={{ color: "#0099ff" }} /> */}
+                {/* <FontAwesomeIcon icon={faTimesCircle} size="3x" style={{ color: "#0099ff" }} /> */}
+                {/* <FontAwesomeIcon icon={faTimes} size="3x" style={{ color: "#0099ff" }} /> */}
+                {/* <FontAwesomeIcon icon={["fas", "sign-out-alt"]} fixedWidth /> */}
+              </button>
+        {/* {allUploadedUserList.map((u, i) => (
           <>
             {console.log(u)}
-            <View_Flashcards onlyOne={true} userId={u._id} key={i} />
+            <p>{u._id}</p>
+            {/* <View_Flashcards onlyOne={true} userId={u._id} key={i} /> */}
             {/* <UserInfo userNameInfo={u.name} userId={u._id} key={i} /> */}
             {/* either print my or the user with a possessive */}
             {/* <Link to={"/Flashcards/" + u._id}>
               I want to see all of {this.getPossessive(u._id, this.props.userId, u.name)}{" "}
               flashcards!
             </Link> */}
-            <br />
+            {/* <br />
           </>
-        ))}
+ */} 
+        {this.state.allPhotos.map((p,i) => 
+          <IndividualFlashcard photoFacts={p} ownPhoto={false} onlyOne = {true} hasLooped={false} viewingUserId={this.props.userId} showInNativeLanguage={this.state.showInNativeLanguage}/>
+        )}
       </div>
     );
   }
