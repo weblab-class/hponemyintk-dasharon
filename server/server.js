95a0dd9bd096c4a124c09e59a23f2f9ab7240789
dasharon <dsharon36@gmail.com>
2021-01-27 09:41:27 -0500

    New database WeWorld_2021_01_27_1

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const socketManager = require("./server-socket");
 // TODO change connection URL after setting up your team database
 const mongoConnectionURL = process.env.MONGOOSE_URL;
 // TODO change database name to the name you chose
-const databaseName = "Test_2021_01_25_3";
+const databaseName = "WeWorld_2021_01_27_1";
 
 // connect to mongodb
 mongoose
